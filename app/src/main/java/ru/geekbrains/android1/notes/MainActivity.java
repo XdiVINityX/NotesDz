b4f768e3cae4e1c87ccafa7f08c934df48835fe6
User <>
2022-11-28 22:17:34 +0300

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/android1/notes/MainActivity.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.android1.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.notes_container, new NotesFragment())
+                .commit();
+    }
+}
